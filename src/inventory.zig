d08e6e62174ffab5c8827c8d94fe4ac32d564f7a
redxaxder <abyaly@gmail.com>
2026-03-03 16:26:49 -0600

    toward item attributes

--- src/inventory.zig
+++ src/inventory.zig
@@ -10,6 +10,7 @@ var pending_pickups: usize = 0;
 pub var inventory: [INVENTORY_SIZE]Item = .{Item.DEFAULT} ** INVENTORY_SIZE;
 var item_count: usize = 0;
 var item_capacity: usize = 3;
+var cash: usize = 0;
 
 var next_item: Item = undefined;
 
@@ -24,8 +25,16 @@ pub fn extend_item_capacity(ksize: u8) void {
     item_capacity = @min(item_capacity, INVENTORY_SIZE);
 }
 
+pub fn roll_low(rng: std.Random, rounds: usize, min: i16, max: i16) i16 {
+    var result = max;
+    for (0..rounds) |_| {
+        result = @min(result, rng.intRangeAtMost(i16, min, max));
+    }
+    return result;
+}
+
 pub fn roll_next_item(rng: std.Random) void {
-    const t = blk: while (true) {
+    const t: ItemTag = blk: while (true) {
         const c = rng.int(usize) % std.enums.values(ItemTag).len;
         const x: ItemTag = @enumFromInt(c);
         if (x != .Nil) {
@@ -33,6 +42,22 @@ pub fn roll_next_item(rng: std.Random) void {
         }
     };
     next_item = .tagged(t);
+
+    switch (t) {
+        .Labubu => {
+            next_item.attrs.field(.motorcycle_armor).* +=
+                roll_low(2, 3, 6);
+        },
+        .Gamma_Beam => {
+            next_item.attrs.field(.radioactive_damage).* +=
+                roll_low(4, 2, 10);
+        },
+        .Rifle => {},
+        .Rocket_Launcher => {},
+        .Nil => {
+            unreachable;
+        },
+    }
 }
 
 pub fn add_pending_pickup() void {
@@ -61,23 +86,41 @@ pub fn overwrite_slot(rng: std.Random, slot: usize) void {
 
 pub const ItemTag = enum {
     Nil,
+
+    // Trinkets
     Labubu,
-    Cell_Phone,
-    Figurine,
-    Pencil_Case,
-    Omamori,
-    Talisman,
-    Amulet,
-    Sakazuki,
-    Hachimaki,
-    Briefcase,
-    Hair_Clip,
-    Juzu_Beads,
-    Credit_Card,
-    Stamp_Seal,
-    Odd_Odometer,
-    Odder_Odometer,
-    Oddest_Odometer,
+    // Cell_Phone,
+    // Figurine,
+    // Pencil_Case,
+    // Omamori,
+    // Talisman,
+    // Amulet,
+    // Sakazuki,
+    // Hachimaki,
+    // Briefcase,
+    // Hair_Clip,
+    // Juzu_Beads,
+    // Credit_Card,
+    // Stamp_Seal,
+    // Odd_Odometer,
+    // Odder_Odometer,
+    // Oddest_Odometer,
+
+    // Weapons
+    Rifle,
+    Rocket_Launcher,
+    Gamma_Beam,
+
+    pub fn is_trinket(self: ItemTag) bool {
+        return !self.is_weapon();
+    }
+
+    pub fn is_weapon(self: ItemTag) bool {
+        return switch (self) {
+            .Rifle, .Rocket_Launcher, .Gamma_Beam => true,
+            else => false,
+        };
+    }
 
     pub fn name(self: ItemTag) []const u8 {
         if (self == .nil) {
@@ -95,13 +138,64 @@ pub const ItemTag = enum {
     }
 };
 
+pub const Attribute = enum {
+    radioactive_damage,
+    explosion_radius,
+    explosion_damage,
+    gun_damage,
+    gun_combo,
+    crit3_bonus,
+    crit4_bonus,
+    crit5_bonus,
+    motorcycle_damage,
+    motorcycle_armor,
+    top_speed,
+    acceleration,
+};
+
+const NUM_ATTRIBUTES: usize = std.enums.values(Attribute).len;
+pub const Attributes = struct {
+    data: [NUM_ATTRIBUTES]i16 = .{0} ** NUM_ATTRIBUTES,
+
+    pub const ZERO: Attributes = .{};
+
+    pub fn pluseq(self: *Attributes, rhs: *const Attributes) void {
+        for (&self.data, rhs.data) |*s, r| {
+            s.* += r;
+        }
+    }
+
+    pub fn plus(self: *const Attributes, rhs: *const Attributes) Attributes {
+        var result = self.*;
+        result.pluseq(rhs);
+        return result;
+    }
+
+    pub fn field(self: *Attributes, attr: Attribute) *i16 {
+        const ix: usize = @intFromEnum(attr);
+        return &self.data[ix];
+    }
+    pub fn effective_value(self: *Attributes, attr: Attribute) i16 {
+        return self.field(attr) + bonuses().field(attr);
+    }
+};
+
+pub fn bonuses() Attributes {
+    const n = inventory_first_free() orelse item_capacity;
+    var accum: Attributes = .ZERO;
+    for (0..n) |ix| {
+        const item = inventory[ix];
+        if (item.tag.is_trinket()) {
+            accum.pluseq(item.attrs);
+        }
+    }
+    return accum;
+}
+
 pub const Item = struct {
     // universal
     tag: ItemTag = .Nil,
-    // in_inventory: bool = false,
-    // position: IVec2 = .{ .x = 0, .y = 0 },
-
-    //trinkets
+    attrs: Attributes = .ZERO,
 
     pub const DEFAULT: Item = .{};
 
